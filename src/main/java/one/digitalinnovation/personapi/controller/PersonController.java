b9e1c4c0ef14f511766e468b38028351d7617247
Pedro Henrique Costa Brandão <pedro@pedrobrandao.com.br>
2021-04-29 14:46:42 -0300

    Implement create and findAll

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,16 +1,38 @@
 package one.digitalinnovation.personapi.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonService;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-	
+
+	private PersonService personService;
+
+	public PersonController(PersonService personService) {
+		this.personService = personService;
+	}
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public MessageResponseDTO createPerson(@RequestBody Person person) {
+		return personService.createPerson(person);
+	}
+
 	@GetMapping
-	public String getBook() {
-		return "API Test!";
+	public List<Person> listPerson() {
+		return personService.findAllPerson();
 	}
 
 }
