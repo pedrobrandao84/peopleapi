2c0dffddc120b92f369f2a9d654f3dc5199a896b
Pedro Henrique Costa Brandão <pedro.costa.brandao@avanade.com>
2021-04-29 11:07:26 -0300

    Ajusta versionamento rest

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/vi/people")
+@RequestMapping("/api/v1/people")
 public class PersonController {
 	
 	@GetMapping
